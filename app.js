bc51cf9b3b951e323717b5345dea831228567d41
Saira Batool <sairabatool787@gmail.com>
2024-05-16 05:56:39 +0500

    INprocess

---
+++ app.js
@@ -0,0 +1,11 @@
+function calculateBMI(){
+    var inputheightfeet = document.getElementById("inputheightfeet").value;
+    var inputheightinch = document.querySelector("#inputheightinch").value;
+    // both method are true 
+
+    var inputweight = document.querySelector("#inputweight").value;
+
+    inputheightfeet = parseInt();
+    //Number(se bhi ker saktye hain) but humayen float mai chahiye abhi
+
+}
